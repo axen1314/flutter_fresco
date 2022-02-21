b1551e7d9df7af095f175736403a7134a39fdc80
axen <g>
2022-02-21 17:09:07 +0800

    Updated 支持gif、webp

--- example/android/app/src/main/java/org/axen/flutter/fresco/flutter_fresco_example/App.java
+++ example/android/app/src/main/java/org/axen/flutter/fresco/flutter_fresco_example/App.java
@@ -2,12 +2,28 @@ package org.axen.flutter.fresco.flutter_fresco_example;
 
 import android.app.Application;
 
+import com.facebook.animated.giflite.GifDecoder;
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imageformat.DefaultImageFormats;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
+import com.facebook.imagepipeline.core.ImageTranscoderType;
+import com.facebook.imagepipeline.core.MemoryChunkType;
+import com.facebook.imagepipeline.decoder.ImageDecoderConfig;
 
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Fresco.initialize(this);
+        ImageDecoderConfig decoderConfig = ImageDecoderConfig.newBuilder()
+                .overrideDecoder(DefaultImageFormats.GIF, new GifDecoder())
+                .build();
+        ImagePipelineConfig pipelineConfig = ImagePipelineConfig.newBuilder(this)
+                .setMemoryChunkType(MemoryChunkType.BUFFER_MEMORY)
+                .setImageTranscoderType(ImageTranscoderType.JAVA_TRANSCODER)
+                .setImageDecoderConfig(decoderConfig)
+                .experiment()
+                .setNativeCodeDisabled(true)
+                .build();
+        Fresco.initialize(this, pipelineConfig);
     }
 }
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String url = "http://img.netbian.com/file/2020/0904/7cab180eca805cce596b6870cb4e1379.jpg";
+  String gifUrl = "http://wx2.sinaimg.cn/large/ceeb653ely1fnfri4s099g205004r7rw.gif";
 
   @override
   void initState() {
@@ -28,8 +29,8 @@ class _MyAppState extends State<MyApp> {
         body: Center(
             child: Column(
               children: [
-                Image.network(url, width: 200, height: 200),
-                Fresco.network(url, width: 200, height: 200),
+                Image.network(gifUrl, width: 200, height: 200),
+                Fresco.network(gifUrl, width: 200, height: 200),
                 Fresco.drawable("R.drawable.ic_bar_back", width: 24, height: 24)
               ],
             )
